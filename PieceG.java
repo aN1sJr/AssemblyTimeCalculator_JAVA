69adb8625dca6f4d2e74b79ed8aa145a3bfb9b09
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-05-14 15:48:42 +0100

    piece g

---
+++ PieceG.java
@@ -0,0 +1,17 @@
+package TP;
+
+public class PieceG {
+	
+	protected int time;
+	
+	public PieceG(int t) {
+		this.time=t;
+	}
+	
+	public int fab_time() {
+		return time;
+	}
+	
+	public int get_time() {return time;}
+	public void set_time(int t) {this.time=t;}
+}
